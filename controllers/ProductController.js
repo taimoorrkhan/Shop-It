2512521325f4be801bf9cfb84219480301c77f86
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-07-24 12:25:54 +0500

    added product controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,3 +1,19 @@
+const Product = require('../models/product')
+
+//Creating Product
+
+exports.AddProduct = async (req, res, next) => {
+  
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product
+  })
+
+
+}
+
+
 exports.getProduct = (req, res) => {
   res.status(200).json({
     success: true,
