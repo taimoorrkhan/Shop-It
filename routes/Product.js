fde3bd06654068e7294177f2ac00340b9401bd79
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-07-24 12:25:27 +0500

    added product route

--- routes/Product.js
+++ routes/Product.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProduct } = require('../controllers/ProductController');
+const { getProduct, AddProduct } = require('../controllers/ProductController');
 
 
-router.route('/').get(getProduct);
+router.route('/products').get(getProduct);
+
+router.route('/product/new').post(AddProduct);
 
 module.exports = router;
